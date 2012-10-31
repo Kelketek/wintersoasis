6f45d7cc65a0d614b3a619747e3be8a90cd2dc1c
Naraka Development <production@naraka.wintersoasis.com>
2012-10-31 05:37:13 +0000

    Forgot to add lib/oasis.py on last commit.

--- lib/oasis.py
+++ lib/oasis.py
@@ -1,18 +1,19 @@
 import ev
-from constants import *
 from ev import utils
 from src.server.sessionhandler import SESSIONS
 
-def partial_pmatch(name):
+def partial_pmatch(me, name):
     """
-        Feed this a name, and it will return character matches. It will return only
-    one match if the name is exact, and it will return multiple if there are multiple
-    people online who match, but none of which are exact matches.
-        All results are returned in a list. An empty list is returned if there is no match.
+        Feed this a name, and it will return character matches. It will return
+    only one match if the name is exact or unambiguous, and it will return
+    multiple if there are multiple people online who match, but none of which
+    are exact matches. All results are returned in a list. An empty list is
+    returned if there is no match.
+        Me is the object that will be returned if the query is 'me'.
     """
-    target = NEXUS.search("*" + name, global_search = True)
+    target = me.search("*" + name, global_search=True, ignore_errors=True)
     if target:
-        return [ target ]
+        return  target
     matches = []
     for session in SESSIONS.sessions.values():
         if session.get_character().name.lower().startswith(name.lower()):
