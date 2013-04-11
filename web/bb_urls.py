ef6fee4697ac47a9f7dcd7b5c2bee30e09fbcca6
Naraka Development <production@naraka.wintersoasis.com>
2013-04-11 10:49:05 -0500

    Quick patch to bb_urls. There are better ways to handle this, and I should replace this method eventually.

--- web/bb_urls.py
+++ web/bb_urls.py
@@ -50,21 +50,17 @@ urlpatterns = patterns('',
         'template': 'djangobb_forum/profile/profile_personal.html'
         }, name='forum_profile_personal'),
     url('^user/(?P<username>.*)/essentials/$', forum_views.user, name='forum_profile_essentials'),
-    url('^user/(?P<username>.*)/$', redirect_to, {'url': '/character/profile/%(username)s/'}, name='forum_profile'),
+    url('^user/(?P<username>.*)/$', forum_views.user, name='forum_profile'),
     url('^users/$', forum_views.users, name='forum_users'),
-
     # Topic
     url('^topic/(?P<topic_id>\d+)/$', forum_views.show_topic, name='topic'),
-    url('^(?P<forum_id>\d+)/topic/add/$', forum_views.add_post,
-        {'topic_id': None}, name='add_topic'),
+    url('^(?P<forum_id>\d+)/topic/add/$', forum_views.add_topic, name='add_topic'),
     url('^topic/(?P<topic_id>\d+)/delete_posts/$', forum_views.delete_posts, name='delete_posts'),
     url('^topic/move/$', forum_views.move_topic, name='move_topic'),
     url('^topic/(?P<topic_id>\d+)/stick_unstick/(?P<action>[s|u])/$', forum_views.stick_unstick_topic, name='stick_unstick_topic'),
     url('^topic/(?P<topic_id>\d+)/open_close/(?P<action>[c|o])/$', forum_views.open_close_topic, name='open_close_topic'),
 
     # Post
-    url('^topic/(?P<topic_id>\d+)/post/add/$', forum_views.add_post,
-        {'forum_id': None}, name='add_post'),
     url('^post/(?P<post_id>\d+)/$', forum_views.show_post, name='post'),
     url('^post/(?P<post_id>\d+)/edit/$', forum_views.edit_post, name='edit_post'),
     url('^post/(?P<post_id>\d+)/delete/$', forum_views.delete_post, name='delete_post'),
@@ -74,13 +70,17 @@ urlpatterns = patterns('',
     # Subscription
     url('^subscription/topic/(?P<topic_id>\d+)/delete/$', forum_views.delete_subscription, name='forum_delete_subscription'),
     url('^subscription/topic/(?P<topic_id>\d+)/add/$', forum_views.add_subscription, name='forum_add_subscription'),
-    
+
     # Feeds
     url(r'^feeds/posts/$', LastPosts(), name='forum_posts_feed'),
     url(r'^feeds/topics/$', LastTopics(), name='forum_topics_feed'),
     url(r'^feeds/topic/(?P<topic_id>\d+)/$', LastPostsOnTopic(), name='forum_topic_feed'),
     url(r'^feeds/forum/(?P<forum_id>\d+)/$', LastPostsOnForum(), name='forum_forum_feed'),
     url(r'^feeds/category/(?P<category_id>\d+)/$', LastPostsOnCategory(), name='forum_category_feed'),
+
+
+    url('^user/(?P<username>.*)/$', redirect_to, {'url': '/character/profile/%(username)s/'}, name='forum_profile'),
+
 )
 
 ### EXTENSIONS ###
@@ -104,3 +104,4 @@ if (forum_settings.ATTACHMENT_SUPPORT):
     urlpatterns += patterns('',
         url('^attachment/(?P<hash>\w+)/$', forum_views.show_attachment, name='forum_attachment'),
     )
+
