9eb0d5c637968f032b74e43196649bd18664affb
Kelketek Rritaa <kelketek@gmail.com>
2013-07-21 16:46:50 -0500

    Removed game time script from startup, tweaked index page.

--- scripts/startstop.py
+++ scripts/startstop.py
@@ -2,7 +2,6 @@ from src.utils.create import create_script
 
 def at_server_start():
     pass
-    create_script("src.utils.gametime.GameTime", obj=None)
 def at_server_stop():
     pass
 def at_server_reload():
